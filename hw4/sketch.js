dbfb4e007fd67cb50c170ab2bd2ba528587d8b50
div10nant <divTENNANT@GMAIL.COM>
2026-02-15 22:27:34 -0500

    yes

--- hw4/sketch.js
+++ hw4/sketch.js
@@ -1,10 +1,17 @@
+let canvas;
+
 function setup(){
-    let canvas = createCanvas(800, 800, WEBGL);
+    canvas = createCanvas(600, 600, WEBGL);
+ 
     angleMode(DEGREES);
+    
+    let button = createButton("Refresh Screen");
+    button.parent("button-holder");
+    button.mousePressed(refreshScreen);
 }
 
 function preload() {
-    myModel = loadModel('snakemodel.obj',true);
+   // myModel = loadModel('snakemodel.obj',true);
 
 }
 
@@ -13,9 +20,13 @@ function draw(){
     ambientLight(100);
     pointLight(255,255,255,0,0,500);
     orbitControl();
-    model(myModel);
+    //model(myModel);
+    fill(0);
+    box(50);
     
-    box(50,50,50);
-    
+}
 
+function refreshScreen(){
+    window.location.reload();
+    
 }
