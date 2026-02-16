28f4f74ad7a81ac89a6aadeb9ed9b788b9763648
div10nant <divTENNANT@GMAIL.COM>
2026-02-15 22:18:42 -0500

    lol

--- hw4/sketch.js
+++ hw4/sketch.js
@@ -3,7 +3,19 @@ function setup(){
     angleMode(DEGREES);
 }
 
+function preload() {
+    myModel = loadModel('snakemodel.obj',true);
+
+}
+
 function draw(){
     background(255);
+    ambientLight(100);
+    pointLight(255,255,255,0,0,500);
+    orbitControl();
+    model(myModel);
     
+    box(50,50,50);
+    
+
 }
